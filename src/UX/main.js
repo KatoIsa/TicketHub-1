00713d31d614d875c5f933cfe4b125b0fe2fe5ab
KatoIsa <61096684+KatoIsa@users.noreply.github.com>
2022-11-24 16:22:23 +0300

    Build 3.0
    changed JavaScript version as in fell back to the old one

--- src/UX/main.js
+++ src/UX/main.js
@@ -1,43 +1,66 @@
+// main application functionality.
 
-// site functionality: button responsivness
-function buttonRresponse(){
-	// collect ariables 
-	let signInHome = document.querySelector('.SignInHome ');
-	let signUpHome = document.querySelector('.SignUpHome');
-	let shadow = document.querySelector('.cover');
-	let popup = document.querySelector('.PortalCover');
-	// sign up portal(pop up elements)
-	let formData = document.querySelector('.AccountPortal .center .Sign-In')
-	let signUpButtons = document.querySelectorAll('.AccountPortal .top div')
-	let signIn = signUpButtons[0], signUp = signUpButtons[1];
+let App = {
+	// button response.
+	buttonRresponse: function (){
+		let signInHome = _.Select('.SignInHome ');
+		let signUpHome = _.Select('.SignUpHome');
+		let shadow = _.Select('.cover');
+		let popup = _.Select('.PortalCover');
+		// sign up portal(pop up elements)
+		let formData = _.Select('.AccountPortal .center .Sign-In')
+		let signUpButtons = _.Select('.AccountPortal .top div', true)
+		let signIn = signUpButtons[0], signUp = signUpButtons[1];
 
-	signInHome.addEventListener('click', ()=>{
-		console.log('hello', formData)
-		shadow.classList.add('extend');
-		popup.classList.remove('hidePortal');
-		popup.classList.add('showPortal');
-	})
-	signUpHome.addEventListener('click', ()=>{
-		shadow.classList.add('extend');
-		popup.classList.remove('hidePortal');
-		popup.classList.add('showPortal');
+		_.Event(signInHome, 'click', function (){
+			shadow.classList.add('extend');
+			popup.classList.remove('hidePortal');
+			popup.classList.add('showPortal');
+			_.Select('.AccountPortal .Title2 h2').classList.add('Active')
+			_.Select('.AccountPortal .Title h2').classList.remove('Active')
+			formData.classList.add('displaySignIn')
+		}, true)
+		
+		_.Event(signUpHome, 'click', function (){
+			shadow.classList.add('extend');
+			popup.classList.remove('hidePortal');
+			popup.classList.add('showPortal');
+			_.Select('.AccountPortal .Title h2').classList.add('Active')
+			_.Select('.AccountPortal .Title2 h2').classList.remove('Active')
+			formData.classList.remove('displaySignIn')
+		}, true)
 
-	})
+		_.Event(shadow, 'click', function(){
+			popup.classList.remove('showPortal');
+			popup.classList.add('hidePortal');
+			shadow.classList.remove('extend');
+		}, true)
 
-	shadow.addEventListener('click', ()=>{
-		popup.classList.remove('showPortal');
-		popup.classList.add('hidePortal');
-		shadow.classList.remove('extend');
+		// sign in and sign up pop up functionality
+		_.Event(signIn, 'click', function(){
+			formData.classList.add('displaySignIn')
+			_.Select('.AccountPortal .Title2 h2').classList.add('Active')
+			_.Select('.AccountPortal .Title h2').classList.remove('Active')
+		}, true)
+		
+		_.Event(signUp, 'click', function(){
+			formData.classList.remove('displaySignIn')
+			_.Select('.AccountPortal .Title2 h2').classList.remove('Active')
+			_.Select('.AccountPortal .Title h2').classList.add('Active')
+		}, true)
+	},
 
-	})
-
-	// sign in and sign up pop up functionality
-	signIn.addEventListener('click', ()=>{
-		formData.classList.add('displaySignIn')
-	})
-	signUp.addEventListener('click', ()=>{
-		formData.classList.remove('displaySignIn')
-	})
-
+	// user acount and database configuron for user login
+	UserAccountAndDbsConfiguration: function () {
+		// chek user inputs: sign up section.
+		let submitButton = _.Select('.submit')
+		let inputs  = _.Select('.Sign-Up div input', true);
+		
+		_.Event(submitButton, 'click', function(){
+			alert('clicked');
+		})
+	}
 }
-buttonRresponse()
+
+App.buttonRresponse();
+App.UserAccountAndDbsConfiguration();
---
+++ src/UX/simplify(js)/Simplified.js
@@ -0,0 +1,156 @@
+/*! Fluid-inc - Simplified.js
+this is a javascript helper library which helps writing less code
+making your code more readable and accesible.
+
+Copyright (c) 2020-2021 Fluid-inc Corp. All rights reserved.
+
+All portions of this file are the confidential and proprietary
+intellectual property of Fluid-inc Corp.
+
+Author : KATO ISA AKA-(ETHAN), Africa (Uganda-Kampala),
+tell / whatsApp  :  +256 705207718.
+    website: https://katoisa256.ga/
+    facebook: Ethan Snowden.
+*/
+/** 
+* Simplify.js Main core: edit on ur own risk: Author KatoIsa
+*  https://katoisa256.ga/ ...
+*/
+let _ = {
+    /** 
+    *Simple selector helpers: by KatoIsa
+    ** It has two parameters /element/ and /MultiElements/
+    * @param {*} element add the HTML element of choice
+    * but remember if u want to select more than one HTML elements at once
+    * add the second parameter and sate it to true 
+    * ```js
+    * Exmaple _.Select('el', true).
+    * ```
+    * @param {*} MultiElements must be sate to true to enable iteration 
+    * through multi HTML elements.
+    */
+    Select: (element, MultiElements = false) => {
+        /* single Elements */
+        if(MultiElements){
+        return document.querySelectorAll(element);
+        }else{
+            return document.querySelector(element);
+        }
+    },
+    /**
+     * ForEach selector, write less code, biuld powerful apps
+     ** Its has three parameters{/elemnt/, /AssignedName/, /Varaible/}
+     * @param {*} Elelemt  HTML element in use (string or varable)
+     * @param {*} AssignedName  variable name of choice
+     * @param {*} Type  type of paremetr?(variable = true; string = false), 
+     ** javascript will through an error in thie console if thie first (Element)
+     ** parameter is a variable and last parameter is not sate to true
+     ** 
+     ** the program will check to see * if thie parameter is a string or varaible
+     ** If its a string it will use its own html element Selector
+     ** If it is a vriable thie program will read thie data the varaible stores
+     ** and check to see if the the last paremeter is sate to true.
+     
+     */
+    Each: (element,AssignedName, Type = false)=>{
+        if(Type){
+            element.forEach(AssignedName);
+        }else{
+        _.Select(element, true).forEach(AssignedName);
+        }
+    },
+    /** 
+    *Simple Eventlistener function: /by KatoIsa/
+    ** thie function has four parameters.
+    ** thie main and last parameter is a boolean
+    ** If you have sate a variable instead of an html element('string'), you will get an error
+    ** If u do get an error, add thie fourth parameter as a boolean
+    **Code Example:
+    *
+    * ```js
+    * _.Event('.button', 'click', ()=>{
+    *    alert('hello wolrd');
+    * }, true);
+    * ```
+    * 
+    * 
+
+    */
+    Event: (element, EventType, Task, Type = false) => {
+        
+        if(Type){
+          return  element.addEventListener(EventType, Task);
+        }else{
+           return _.Select(element).addEventListener(EventType, Task);
+        }
+        
+    },
+    /**For Each Click Event ..
+    ** Thie last parameter should be sate to true if ur using a varable
+    ** instead of a string eg('.htmleleemnts') istead of (Type);
+    */
+    EventAll: (element, EventType, Task, Type = false) => { 
+        if(Type){
+         return element.forEach(f =>{
+                f.addEventListener(EventType, Task);
+            });
+        }else{
+        return _.SelectAll(element).forEach(f => {
+                f.addEventListener(EventType, Task)
+            });
+        }
+    },
+    /** 
+    *Helps create html elements[tags... etc]: by KatoIsa
+    */
+    HTMLcreate: (type)=>{ 
+        return document.createElement(type);
+    },
+    /**
+     * Simple console log function.
+     * not that defferent from Python
+     * ```js
+     * _.Print('hello world');
+     * ```
+     */
+    Print: (value)=>{
+        console.log(value);
+    },
+    /** 
+    *Simple DataBase managent function: by KatoIsa
+    */ 
+    DB: {
+        /**
+         # Write to data base 
+        */
+        Create: (primaryKey, data) => {
+            return localStorage.setItem(primaryKey, data);
+        },
+        /**
+         # collect data from data base 
+         ** it has two parameters --
+         * @param {} primaryKey String containing any name of choice
+         * @param {} Print Sate to true to print data in console
+         * its initial value is sate to false meaning it should do anything becouse it isnt sate, 
+         * If you want to print it out you sate the parameter to true.
+         ** For Example: 
+         * ```js
+         * _.DB.Get('databasename',true);//contents will be printed in console.
+         * ```
+        */
+        Get: (primaryKey, Print = false) => {
+            if (Print) {
+                return console.log(localStorage.getItem(primaryKey));
+            } else {
+                return localStorage.getItem(primaryKey);
+            }
+        },
+        /**
+         * Edit ur data from thie dataBase 
+         ** change content the dataBase holds or update it.
+        */
+        Edit: (primaryKey, newData) => {
+            return localStorage.setItem(primaryKey, newData);
+        }
+    },
+}
