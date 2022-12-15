13b5cacba612801bb4852484b5bcb8b2d41317a6
KatoIsa <61096684+KatoIsa@users.noreply.github.com>
2022-12-15 10:28:18 -0800

    ui update

--- src/UX/user.js
+++ src/UX/user.js
@@ -5,6 +5,8 @@ let User = {
 		let userAccount = _.Select('.userAccount');
 		let signUpHome = _.Select('.SignUpHome');
 		let shadow = _.Select('.cover');
+		let BuyPopUpbutton = _.Select('arrowBody');
+		let buyPopUpWindow = _.Select('paymentsetup');
 
 		_.Event('.TicketsMenuButton', 'click', function(){
 			_.Select('.TicketContentsBody').classList.add('showTickets');
@@ -34,8 +36,14 @@ let User = {
 		_.Event(shadow, 'click', function(){
 			shadow.classList.remove('extend');
 			_.Select('.userAccount ol').classList.remove('appendMenu');
+			buyPopUpWindow.classList.remove('showpaymentsetup');
+
 		}, true);
 
+		_.Event(BuyPopUpbutton, 'click', function(){
+			buyPopUpWindow.classList.add('showpaymentsetup');
+			shadow.classList.add('extend');
+		}, true);
 		
 	},
 	UserAreaActions: function (){
