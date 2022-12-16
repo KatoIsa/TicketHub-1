88f0ce1f351c37c36e507a869a573079dae9c7f9
KatoIsa <61096684+KatoIsa@users.noreply.github.com>
2022-12-16 02:58:46 -0800

    kill

--- src/pages/ADMIN/admin.js
+++ src/pages/ADMIN/admin.js
@@ -20,6 +20,32 @@ let User = {
 			_.Select('.userAccount ol').classList.remove('appendMenu');
 		}, true)
 	},
+	AccountHandler(){
+		let AdminRefer = db.collection("ADMIN").doc('0704465049');
+			
+			AdminRefer.get().then((doc) => {
+				if (doc.exists) {
+					let jsondata = JSON.stringify(doc.data());
+					let getjsondata = JSON.parse(jsondata);
+					// check if valid?
+					if(getjsondata.Authr = true){
+						// Get the document reference
+						const docRef = firestore.doc('users');
+
+						// Get all collections in the document
+						docRef.listCollections().then(collections => {
+							console.log(collections);
+						});
+					}else{
+						window.location.assign('index.html');
+					}
+				}else{
+                    console.log('error cant retrieve data..');
+                }
+			}).catch((error) => {
+				console.log("Error getting document:", error);
+			});
+	}
 }
 
 User.buttonRresponse();
